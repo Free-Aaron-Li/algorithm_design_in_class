f6b88a274e2d9af07652a4fc21804f20c5617a44
aaron <fly_aaron.li@outlook.com>
2023-09-26 20:36:19 +0800

    update

---
+++ chessboard/src/main.cpp
@@ -0,0 +1,9 @@
+//
+// This 'main' belongs to the 'algorithm_design',part of the 'chessboard' project
+//
+// Created by aaron on 23-9-26.
+// Copyright (c) 2023 aaron. All rights reserved.
+//
+// The source file follows the GPL3.0 protocol and is distributed with the file.
+// If you do not receive the GPL3.0 protocol ontology or you find a bug,
+// please let me know (email: fly_aaron.li@outlook.com)
